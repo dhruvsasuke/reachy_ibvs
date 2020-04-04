e566267624db8c7a36cc828fa22d009b708a8e36
Siddhant Saoji <sziddhant@gmail.com>
2020-04-04 17:10:24 +0530

    removedd built files

--- build/reachy_vel_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv/reachy_vel/src/reachy_vel_control
-# Build directory: /home/dhruv/reachy_vel/build/reachy_vel_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
