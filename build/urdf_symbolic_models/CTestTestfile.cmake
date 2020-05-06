2f7e960d41f0c4d38cf782dfe442ea5b36fdc67e
Siddhant Saoji <sziddhant@gmail.com>
2020-05-06 11:34:38 +0530

    Removed makefiles

--- build/urdf_symbolic_models/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv/reachy_vel/src/urdf_symbolic_models
-# Build directory: /home/dhruv/reachy_vel/build/urdf_symbolic_models
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
